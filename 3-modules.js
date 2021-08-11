25a96d70eca5e6c0f93323ca4e2cedcddb62236f
gonzalezmassini <gonzalez.massini@gmail.com>
2021-08-11 12:22:01 -0400

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,14 @@
+//Modules
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+const names = require('./4-names');
+// destructure
+// const { john, peter } = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade');
+console.log(data);
+console.log(names);
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRETE';
+//share
+const john = 'john';
+const peter = 'peter';
+
+module.exports = { john, peter };
